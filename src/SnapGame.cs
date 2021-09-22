58301d65e317c0734bbc408de21cb3016dd1fe55
AurnobSYC <48465861+AurnobSYC@users.noreply.github.com>
2021-09-22 23:14:32 +0600

    Update SnapGame.cs

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -10,7 +10,9 @@ namespace CardGames
         {
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
-            SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
+            SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);
+	    SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 12);
+      // set the cells in the bitmap to match the cards
         }
 
 		/// <summary>
@@ -26,6 +28,23 @@ namespace CardGames
 			{
 				myGame.FlipNextCard ();
 			}
+
+if (myGame.IsStarted)
+{
+if ( SwinGame.KeyTyped (KeyCode.vk_LSHIFT) && SwinGame.KeyTyped (KeyCode.vk_RSHIFT))
+{
+//TODO: add sound effects
+}
+else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT))
+{
+myGame.PlayerHit (0);
+}
+else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT))
+{
+myGame.PlayerHit (1);
+}
+}
+}
 		}
 
 		/// <summary>
@@ -40,9 +59,9 @@ namespace CardGames
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, "GameFont", 0, 20);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, "GameFont", 0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, "GameFont", 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
 			}
 			else
